fa582cf20bd8ee39803be18fd69e95649cd69e07
victorm0612 <rojasvictorvargas@gmail.com>
2020-11-25 17:04:54 -0500

    añadiendo archivos al gitignore.

--- api/config/prod.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    posgresqlURI: 'postgres://ryxkkwzhlqvoxz:8e8d2f5d8d0a19ab192e629960fdef8b1a58f73878ec8e904a65d590805e94ef@ec2-54-85-13-135.compute-1.amazonaws.com:5432/d1n3ttgo8gsrp0',
-};
