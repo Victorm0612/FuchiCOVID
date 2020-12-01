ab4b2044fef0b4e83d46e9c0e9f229604c7158aa
victorm0612 <rojasvictorvargas@gmail.com>
2020-12-01 02:03:44 -0500

    se añade el crud de visitas, pacientes, medicamentos

--- api/src/controllers/index.controller.js
+++ api/src/controllers/index.controller.js
@@ -3,7 +3,8 @@ const { Pool } = require('pg')
 const keys = require(path.join(__dirname, '../config/keys'));
 const jwt = require('jsonwebtoken');
 const express = require('express')
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const { prependOnceListener } = require('process');
 app = express()
 app.use(cookieParser(process.env.COOKIE_SECRET))
 
@@ -30,7 +31,7 @@ const login = async(req, res) => {
         const isSecure = req.app.get('env') != 'development';
 
         res.cookie('JWT_TOKEN', token, { httpOnly: true, secure: isSecure, signed: true, sameSite: true, expires: false, maxAge: 60 * 60 * 1000 })
-        res.status(200).json({ token: token, refreshToken: refreshtoken, id: id_user, type: type_user })
+        res.status(200).json({ refreshToken: refreshtoken, id: id_user, type: type_user })
 
     } else {
         // si son nulos entonces los datos son incorrectos.
@@ -106,7 +107,8 @@ const createPro = async(req, res) => {
             res.status(403).json({ 'RES': 'ERROR DE USUARIO' })
         else {
             // Finaliza validación del token -----
-            const { num_id, tipo_id, nombre, direccion, barrio, registrado_por, universidad, entidadSalud, email, contrasenia } = req.body;
+            const { num_id, tipo_id, nombre, direccion, barrio, universidad, entidadSalud, email, contrasenia } = req.body;
+            const registrado_por = user.id
             let response = await pool.query(`SELECT * FROM crearUniversidadyEntidad('${universidad}','${entidadSalud}')`)
             const id_universidad = response.rows[0].id_u;
             const id_entidadSalud = response.rows[0].id_e;
@@ -140,15 +142,16 @@ const updatePro = async(req, res) => {
                 nombre,
                 direccion,
                 barrio,
-                registrado_por,
                 universidad,
                 entidadSalud,
                 email,
                 contrasenia
             } = req.body;
+            const registrado_por = user.id
             let response = await pool.query(`SELECT * FROM crearUniversidadyEntidad('${universidad}','${entidadSalud}')`)
             const id_universidad = response.rows[0].id_u;
             const id_entidadSalud = response.rows[0].id_e;
+
             try {
                 const response = await pool.query(
                     `UPDATE profesional SET tipo_id = '${tipo_id}', nombre_profesional='${nombre}', direccion = '${direccion}', barrio='${barrio}', registrado_por=${registrado_por}, id_universidad=${id_universidad}, id_entidadSalud=${id_entidadSalud}, email='${email}', contrasenia='${contrasenia}' WHERE num_id = '${num_id}'`
@@ -240,9 +243,9 @@ const createPacient = async(req, res) => {
             res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
         else {
             // Finaliza validación del token -----
-            const { num_id, tipo_id, nombre_completo, direccion, ciudad, id_doctor, geolocalizacion, num_personas_convivencia } = req.body
+            const { num_id, tipo_id, nombre_completo, direccion, ciudad, barrio, id_doctor, geolocalizacion, num_personas_convivencia, hipotesis } = req.body
             try {
-                pool.query(`INSERT INTO paciente(num_id, tipo_id, nombre_completo, direccion, ciudad, id_doctor, geolocalizacion, num_personas_convivencia) VALUES('${num_id}', '${tipo_id}', '${nombre_completo}', '${direccion}', '${ciudad}', '${id_doctor}', '${geolocalizacion}', '${num_personas_convivencia}')`);
+                pool.query(`INSERT INTO paciente(num_id, tipo_id, nombre_completo, direccion, ciudad, barrio, id_doctor, geolocalizacion, num_personas_convivencia, hipotesis) VALUES('${num_id}', '${tipo_id}', '${nombre_completo}', '${direccion}', '${ciudad}', '${barrio}', '${id_doctor}', '${geolocalizacion}', '${num_personas_convivencia}', '${hipotesis}')`);
                 res.json(req.body);
             } catch (error) {
                 res.json({ 'RES': 'ERROR AL REGISTRAR PROFESIONAL' + error });
@@ -262,9 +265,9 @@ const updatePacient = async(req, res) => {
             res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
         else {
             // Finaliza validación del token -----
-            const { num_id, tipo_id, nombre_completo, direccion, ciudad, id_doctor, geolocalizacion, num_personas_convivencia } = req.body
+            const { num_id, tipo_id, nombre_completo, direccion, ciudad, barrio, id_doctor, geolocalizacion, num_personas_convivencia, hipotesis } = req.body
             try {
-                pool.query(`UPDATE paciente SET num_id='${num_id}', tipo_id='${tipo_id}', nombre_completo='${nombre_completo}', direccion='${direccion}', ciudad='${ciudad}', id_doctor='${id_doctor}', geolocalizacion='${geolocalizacion}', num_personas_convivencia='${num_personas_convivencia}'`);
+                pool.query(`UPDATE paciente SET tipo_id='${tipo_id}', nombre_completo='${nombre_completo}', direccion='${direccion}', ciudad='${ciudad}',barrio='${barrio}' id_doctor='${id_doctor}', geolocalizacion='${geolocalizacion}', num_personas_convivencia='${num_personas_convivencia}',hipotesis='${hipotesis}' WHERE num_id='${num_id}'`);
                 res.json({ 'RES': 'ACTUALIZADO' })
             } catch (error) {
                 res.status(403).json({ 'RES': 'ERROR AL ACTUALIZAR PACIENTE ' + error })
@@ -299,6 +302,245 @@ const deletePacient = async(req, res) => {
     }
 };
 
+const getContacts = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const response = await pool.query('SELECT * FROM contacto');
+            res.json(response.rows);
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR TOKEN VACIO' })
+    }
+};
+
+const getContactById = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null || user.type === 2)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const id = req.params.id
+            try {
+                const response = await pool.query(`SELECT * FROM contacto WHERE id_contacto='${id}'`);
+                res.json(response.rows)
+            } catch (error) {
+                res.status(403).json({ 'RES': 'ERROR, NO SE ENCUENTRA EL PROFESIONAL' })
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const createContact = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const { nombre, paciente_id, parentesco, telefono, email } = req.body
+            try {
+                pool.query(`SELECT addContacto('${paciente_id}','${nombre}','${parentesco}','${telefono}','${email}', 0)`)
+                res.json(req.body);
+            } catch (error) {
+                res.json({ 'RES': 'ERROR AL REGISTRAR CONTACTO ' + error });
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const updateContact = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const { nombre, paciente_id, parentesco, telefono, email, id_contacto } = req.body
+            try {
+                pool.query(`SELECT addContacto('${paciente_id}','${nombre}','${parentesco}','${email}','${telefono}','${id_contacto}')`)
+                res.json(req.body);
+            } catch (error) {
+                res.json({ 'RES': 'ERROR AL ACTUALIZAR CONTACTO ' + error });
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const deleteContact = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const id = req.params.id;
+            try {
+                await pool.query(
+                    `DELETE FROM contacto WHERE id_contacto = '${id}'`
+                );
+                res.json({ 'RES': 'BORRADO' });
+            } catch (error) {
+                res.json({ 'RES': 'NO SE ENCUENTRA ' + error });
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const getVisita = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const response = await pool.query('SELECT * FROM visita');
+            res.json(response.rows);
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR TOKEN VACIO' })
+    }
+};
+
+const createVisita = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null || user.id === 1)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const id_doctor = user.id;
+            const { id_paciente, id_medicamento, temperatura, peso, presion, dosis, observacion } = req.body
+            try {
+                pool.query(`INSERT INTO visita(id_doctor, id_paciente, id_medicamento, temperatura, peso, presion,dosis,observacion) VALUES('${id_doctor}','${id_paciente}','${id_medicamento}','${temperatura}','${peso}','${presion}','${dosis}','${observacion}')`)
+                res.json(req.body);
+            } catch (error) {
+                res.json({ 'RES': 'ERROR AL REGISTRAR VISITA ' + error });
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const updateVisita = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const id_doctor = user.id;
+            const { id_visita, id_paciente, id_medicamento, temperatura, peso, presion, dosis, observacion } = req.body
+            try {
+                pool.query(`UPDATE visita SET id_doctor='${id_doctor}', id_paciente='${id_paciente}', id_medicamento='${id_medicamento}',temperatura='${temperatura}',peso='${peso}',presion='${presion}',dosis='${dosis}',observacion='${observacion}' WHERE id_visita='${id_visita}'`);
+                res.json(req.body);
+            } catch (error) {
+                res.json({ 'RES': 'ERROR AL ACTUALIZAR VISITA ' + error });
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const deleteVisita = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const id = req.params.id;
+            try {
+                await pool.query(
+                    `DELETE FROM visita WHERE id_visita = '${id}'`
+                );
+                res.json({ 'RES': 'BORRADO' });
+            } catch (error) {
+                res.json({ 'RES': 'NO SE ENCUENTRA LA VISITA ' + error });
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const getMedicamentos = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            try {
+                const response = await pool.query('SELECT * FROM gasto_medicamento');
+                res.json(response.rows)
+            } catch (error) {
+                res.status(403).json({ 'RES': 'ERROR, NO SE ENCUENTRA LOS MEDICAMENTOS' })
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+const updateMedicamento = async(req, res) => {
+    //Validando que el token sea correcto ------
+    const token = req.signedCookies.JWT_TOKEN
+    if (token) {
+        const user = await validateToken(token, process.env.JWT_SECRET);
+        if (user === null)
+            res.status(403).json({ 'RES': 'ERROR TOKEN INVALIDO' })
+        else {
+            // Finaliza validación del token -----
+            const { id_gasto, id_med, id_lab, reserva } = req.body
+            try {
+                pool.query(`UPDATE gasto_medicamento SET id_med='${id_med}', id_lab='${id_lab}',reserva='${reserva}' WHERE id_gasto='${id_gasto}'`);
+                res.json(req.body);
+            } catch (error) {
+                res.json({ 'RES': 'ERROR AL ACTUALIZAR MEDICAMENTO ' + error });
+            }
+        }
+    } else {
+        res.status(403).json({ 'RES': 'ERROR CON TOKEN' })
+    }
+};
+
+
+
 async function validateToken(token, secret) {
     try {
         const result = jwt.verify(token, secret);
@@ -326,5 +568,18 @@ module.exports = {
     getPacientById,
     createPacient,
     updatePacient,
-    deletePacient
+    deletePacient,
+    getContacts,
+    getContactById,
+    createContact,
+    updateContact,
+    deleteContact,
+    getVisita,
+    createVisita,
+    updateVisita,
+    deleteVisita,
+    getMedicamentos,
+    updateMedicamento
+
+
 }
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -13,7 +13,18 @@ const {
     getPacientById,
     createPacient,
     updatePacient,
-    deletePacient
+    deletePacient,
+    getContacts,
+    getContactById,
+    createContact,
+    updateContact,
+    deleteContact,
+    getVisita,
+    createVisita,
+    updateVisita,
+    deleteVisita,
+    getMedicamentos,
+    updateMedicamento
 } = require('../controllers/index.controller')
 
 router.post('/login', login);
@@ -32,4 +43,17 @@ router.post('/registrarpaciente', createPacient);
 router.put('/actualizarpaciente', updatePacient);
 router.delete('/borrarpaciente/:id', deletePacient);
 
+router.get('/contactos', getContacts);
+router.get('/contacto/:id', getContactById);
+router.post('/registrarcontacto', createContact);
+router.put('/actualizarcontacto', updateContact);
+router.delete('/borrarcontacto/:id', deleteContact);
+
+router.get('/visitas', getVisita);
+router.post('/registrarvisita', createVisita);
+router.put('/actualizarvisita', updateVisita);
+router.delete('/borrarvisita/:id', deleteVisita);
+
+router.get('/medicamentos', getMedicamentos);
+router.put('/actualizarmedicamento', updateMedicamento);
 module.exports = router;
